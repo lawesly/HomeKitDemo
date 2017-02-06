c546a759e9a98e14ef70394263f62957a5e86150
XuChengCheng <627051659@qq.com>
2017-02-06 16:44:02 +0800

    Initial Commit

---
+++ HomeKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HomeKitDemo
+//
+//  Created by XuChengcheng on 2017/2/6.
+//  Copyright © 2017年 xcc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
